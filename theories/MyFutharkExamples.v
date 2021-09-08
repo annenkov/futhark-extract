0b97cbeb7ee1601fa4fbe14d1bef76ad4e90371b
Kristian Knudsen Olesen <olesenkk@gmail.com>
2021-09-08 14:50:10 +0200

    Small clean-up and removing string scope

--- theories/MyFutharkExamples.v
+++ theories/MyFutharkExamples.v
@@ -76,11 +76,9 @@ Definition TT_ctors :=
   ; ("true", "true")
   ; ("false", "false")].
 
+Open Scope list_scope.
+
 Module Utils.
-
-  Open Scope list_scope.
-
-  (* Notation "x =s y" := (proj1_sig x = proj1_sig y) (at level 70, no associativity). *)
 
   Class IsMonoid__sig {A : Type} {P : A -> Prop} (op : sig P -> sig P -> sig P) (e : sig P) : Prop :=
     { munit_left__sig  : forall m, proj1_sig (op e m) = proj1_sig m;
@@ -133,22 +131,27 @@ Module Utils.
   Definition reduce {A : Type} (op : A -> A -> A) (e : A) `{IsMonoid A op e} (xs : list A) :=
     fold_right op e xs.
 
-  Theorem reduce_prop:
-    forall (A : Type) (op : A -> A -> A) (ne : A) (isM : IsMonoid A op ne) (l1 l2 : list A),
-      reduce op ne (l1 ++ l2) = op (reduce op ne l1) (reduce op ne l2) /\ reduce op ne [] = ne.
+  (** The reduce function is multiplicative as a monoid homomorphism. *)
+  Theorem reduce_monoid_homo_mult (A : Type) (op : A -> A -> A) (ne : A) `{Utils.IsMonoid A op ne} :
+    forall l1 l2 : list A, Utils.reduce op ne (l1 ++ l2) = op (Utils.reduce op ne l1) (Utils.reduce op ne l2).
   Proof.
-    intros; split; auto;
-      match goal with
-      | |- context[?list1 ++ ?list2] => induction list1 as [|? ? IH]
-      end;
-      simpl.
-    * rewrite munit_left; trivial.
-    * rewrite IH; rewrite massoc; trivial.
+    intros l1 l2; induction l1 as [|? ? IH]; simpl.
+    * rewrite Utils.munit_left; trivial.
+    * rewrite IH; rewrite Utils.massoc; trivial.
   Qed.
 
-End Utils.
+  (** From the multiplicative property of reduce, it behaves as follows with the
+      cons operation. *)
+  Corollary reduce_monoid_homo_cons (A : Type) (op : A -> A -> A) (ne : A) `{Utils.IsMonoid A op ne} :
+    forall (a : A) (l : list A), Utils.reduce op ne (a :: l) = op a (Utils.reduce op ne l).
+  Proof. intros; reflexivity. Qed.
 
-Open Scope string.
+  (** The reduce function preserves the neutral element as a monoid homomorphism *)
+  Theorem reduce_monoid_homo_unit (A : Type) (op : A -> A -> A) (ne : A) `{Utils.IsMonoid A op ne} :
+    Utils.reduce op ne [] = ne.
+  Proof. reflexivity. Qed.
+
+End Utils.
 
 Module Tuples.
   (** Example of how tuples of length greater than two are exported
@@ -171,9 +174,7 @@ Module Tuples.
 
 End Tuples.
 
-
 Module MaximumSegmentSum.
-
   (** Futhark implementation of maximum segment sum:
 
     -- Parallel maximum segment sum
@@ -423,7 +424,7 @@ Module MaximumSegmentSum.
 
   Definition futhark_mss_test :=
     {| FTname := "Maximum segment sum test"
-     ; FTinput := string_of_list (fun n => string_of_Z n ++ "i64") test_input
+     ; FTinput := string_of_list (fun n => string_of_Z n ++ "i64")%string test_input
      ; FToutput := string_of_Z test_output ++ "i64" |}.
 
   MetaCoq Run (futhark_extraction "" mss_prelude
